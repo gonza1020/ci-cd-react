03380947780eb95b9232f1519b91579bcbde4763
gonza1020 <gonzalozoloaga44@gmail.com>
2022-06-01 11:56:45 -0300

    Setting up actions

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function HelloWorld(){
+    return(
+        <div>
+            <h1>Hello world, this is the 2nd test for Ing software</h1>
+            <h2>By gonza</h2>
+        </div>
+    )
+}
+
+export default HelloWorld
