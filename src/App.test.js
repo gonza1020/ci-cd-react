348f9b6d154a55627db1620d359d4611218d6f7c
gonza1020 <gonzalozoloaga44@gmail.com>
2022-06-01 12:24:53 -0300

    breaking test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/gonza/i);
+  const linkElement = screen.getByText(/breaking all/i);
   expect(linkElement).toBeInTheDocument();
 });
