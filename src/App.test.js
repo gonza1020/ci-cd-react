1c7e598d29eb9309999fc442f70d78b0a0b3eb6f
gonza1020 <gonzalozoloaga44@gmail.com>
2022-06-01 13:01:47 -0300

    breaking again with a pul, constraint

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/breaking all/i);
+  const linkElement = screen.getByText(/breaking all again/i);
   expect(linkElement).toBeInTheDocument();
 });
