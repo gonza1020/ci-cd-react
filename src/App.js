b88a4ecacff493f5472dc2de2ec5dc1300287cb8
gonza1020 <81094140+gonza1020@users.noreply.github.com>
2022-06-02 10:54:42 -0300

    Breaking prettier rule

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hola mundo desde corrientes Argentina</p>
+       <p>Hola mundo desde corrientes Argentina</p>
         <HelloWorld />
       </header>
     </div>
