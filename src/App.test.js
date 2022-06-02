996ccbca5b1dc1b1661649dc1b24861a7f212df6
gonza1020 <gonzalozoloaga44@gmail.com>
2022-06-01 23:48:08 -0300

    Adding prettier

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/breaking all again/i);
+  const linkElement = screen.getByText(/gonza/i);
   expect(linkElement).toBeInTheDocument();
 });
